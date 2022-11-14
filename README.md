a9920d6bea0731582c66439524db1fade12462f5
Matija Novosel <mnovosel5@gmail.com>
2022-11-14 16:22:27 +0100

    Comment fetching

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 <h1 align=center>Reddit profile crawler</h1>
 <p align=center>A program that saves a user's profile as a JSON.</p>
+
+## 🚀 Features
+
+- Saves a Reddit users comments and optionally the parent comment or post to a JSON file
+
+## ⚙️ Setup
+
+1. Install the dependencies: `python -m pip install -r requirements.txt`
+2. Create a Reddit app [here](https://www.reddit.com/prefs/apps)
+3. Create an `.env` file with the following properties:
+
+```sh
+client_id=
+client_secret=
+user_agent=
+```
+
+4. Provide the data needed in the `.env` file as prescribed in the [praw](https://praw.readthedocs.io/en/stable/getting_started/quick_start.html) documentation
+
+## ▶️ Options
+
+`crawler.py [OPTIONS]
+
+    -h,                                  Print this help text and exit
+    -u, --user                           The user whose comments will be fetched
+    -c, --context                        Include the parent comment text, providing further context
---
+++ crawler.py
@@ -0,0 +1,66 @@
+import praw
+import os
+import sys, getopt
+import json
+from dotenv import load_dotenv
+
+
+def main(argv):
+    user = ""
+    includeContext = False
+
+    try:
+        opts, args = getopt.getopt(argv, "hu:c", ["user="])
+    except getopt.GetoptError:
+        print(f"crawler.py -u <username> -c")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print(f"crawler.py -u <username> -c")
+            sys.exit()
+        elif opt in ("-u", "--user"):
+            user = arg
+        elif opt in ("-c", "--context"):
+            includeContext = True
+
+    load_dotenv()
+
+    client_secret = os.getenv('client_secret')
+    client_id = os.getenv('client_id')
+    user_agent = os.getenv('user_agent')
+
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent,
+    )
+
+    data = {"user": user, "comments": []}
+    ctr = 0
+
+    for comment in reddit.redditor(user).comments.new(limit=None):
+        parent = comment.parent()
+        obj = {}
+
+        if includeContext:
+            parentText = ""
+            if (isinstance(parent, praw.models.reddit.submission.Submission)):
+                parentText = parent.selftext
+            else:
+                parentText = parent.body
+            obj["parentText"] = parentText
+
+        obj["text"] = comment.body
+        obj["submittedAt"] = comment.created_utc
+        obj["permalink"] = comment.permalink
+
+        data["comments"].append(obj)
+        ctr += 1
+        print(f"Got {ctr} comments!")
+
+    with open("result.json", "w") as f:
+        f.write(json.dumps(data))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
